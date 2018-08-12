db39b2a8c3fc11d64d88a1822c9c43f998f003f3
langa <adam.lang@cdk.com>
2018-08-12 14:31:43 -0700

    all mock sensors now emitting data

--- index.js
+++ index.js
@@ -2,30 +2,42 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 const fetch = require("node-fetch");
+const fs = require("fs");
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
 const app = express();
 
 app.use(index);
 
-const server = http.createServer(app);
-const io = socketIo(server);
+const sensors = JSON.parse(fs.readFileSync(`${process.cwd()}/sensors/sensors.json`, "utf8"));
 
-io.on("connection", socket => {
-  console.log("New client connected");
-  setInterval(() => getApiAndEmit(socket), 1000);
-  socket.on("disconnect", () => console.log("Client disconnected"));
-});
-
-const getApiAndEmit = async socket => {
+const emit = async (socket, sensor) => {
   try {
-    const gyroscope = "http://www.mocky.io/v2/5b6fc5562e00002c00936580";
-    const res = await fetch(gyroscope);
+    const endpointBase = "http://www.adamglang.com/projects/planeSightServer/sensors/";
+    const sensorEndpoint = `${endpointBase}${sensor}.json`;
+    const res = await fetch(sensorEndpoint);
     const json = await res.json();
-    socket.emit("FromAPI", json);
+
+    socket.emit(`From::${sensor}`, json);
   } catch (error) {
-    console.error(`Error: ${error.code}`);
+    console.error(`There was an error from ${sensor}: ${error}`);
   }
 };
 
-server.listen(port, () => console.log(`Listening on port ${port}`));
+for(const [idx, sensor] of sensors.entries()) {
+  const server = http.createServer(app);
+  const socket = socketIo(server);
+  const sensorPort = port + idx;
+  const connectionMessage = `New client connected to ${sensor}`;
+  const disconnectMessage = `Client disconnected from ${sensor}`;
+  const listeningMessage = `Listening for data from ${sensor} on port ${sensorPort}`;
+
+  socket.on("connection", socket => {
+    console.log(connectionMessage);
+    setInterval(() => emit(socket, sensor), 1000);
+  });
+
+  socket.on("disconnect", () => console.log(disconnectMessage));
+
+  server.listen(sensorPort, () => console.log(listeningMessage));
+}
